16a4a22f86ae954053bfc04e1263120c8e6ba100
Runjuu <runjuu@icloud.com>
2017-02-02 00:57:30 +0800

    update demo

--- demo/pages/index.jsx
+++ demo/pages/index.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Calendar from '../../src/';
 import CalendarStyle from '../../style.css';
 import style from './style.sass';
 
-const Demo = () => (
-  <div>
-    <Calendar
-      defaultStyle={CalendarStyle}
-      customizeStyle={style}
-    />
-  </div>
-);
+class Demo extends Component {
+  static handleScroll(e) {
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight * 0.8)) {
+      window.requestAnimationFrame(Calendar.update);
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', Demo.handleScroll);
+  }
+  render() {
+    return (
+      <div>
+        <Calendar
+          defaultStyle={CalendarStyle}
+          customizeStyle={style}
+        />
+      </div>
+    );
+  }
+}
 
 export default Demo;
