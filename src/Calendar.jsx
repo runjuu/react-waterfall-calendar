9692acb027adf86047f90f3e1c76c78c8f86b632
Runjuu <mail@ollie.top>
2017-03-25 17:10:59 +0800

    基础样式完成

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
 import { observer, PropTypes } from 'mobx-react';
+import injectSheet from 'react-jss';
+import moment from 'moment';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  month: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+  horizontal: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+};
 
 @observer
+@injectSheet(styles)
 class Calendar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const date = event.target.getAttribute('href').slice(1);
+    console.log(date);
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
+      <div className={classes.root}>
         {this.props.calendar.map(month => (
-          <div key={month[0][0]}>
+
+          <div key={month[0][0]} className={classes.month}>
+            <h2>{moment(month[1][0]).date(1).format('YYYY-MM')}</h2>
             {month.map(horizontal => (
-              <div key={horizontal[0]}>
+
+              <div key={horizontal[0]} className={classes.horizontal}>
                 {horizontal.map(date => (
-                  <span key={date}>{date}</span>
+                  <a
+                    key={date}
+                    href={`#${date}`}
+                    onClick={this.handleClick}
+                  >
+                    {moment(date).format('DD')}
+                  </a>
                 ))}
               </div>
+
             ))}
           </div>
+
         ))}
       </div>
     );
