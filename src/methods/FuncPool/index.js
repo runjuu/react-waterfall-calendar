6c836508033f96c864546718862fb982810ce4a5
Runjuu <mail@ollie.top>
2017-08-06 11:55:42 +0800

    FuncPool add mocha test

--- src/methods/FuncPool/index.js
+++ src/methods/FuncPool/index.js
@@ -1,23 +1,46 @@
-class FuncPool {
-  constructor() {
-    this.update = this.update.bind(this);
-    this.autoRun = this.autoRun.bind(this);
-    this.removeFromAutoRun = this.removeFromAutoRun.bind(this);
+function checkType(func) {
+  if (func === undefined || typeof func === 'function' || Array.isArray(func)) return func;
+  throw new TypeError('FuncPool accept a function as a parameter');
+}
+
+function FuncPool() {
+  let updatePool = [];
+
+  function update() {
+    const result = [];
+    try {
+      updatePool.forEach(func => typeof func === 'function' && result.push(func()));
+      return Promise.resolve(result);
+    } catch (err) {
+      return Promise.reject(err);
+    }
   }
 
-  updatePool = [];
-
-  update() {
-    this.updatePool.forEach(func => typeof func === 'function' && func());
+  function removeFromAutoRun(func) {
+    checkType(func);
+    const index = updatePool.findIndex(oldFunc => oldFunc === func);
+    if (index > -1) updatePool.splice(index, 1);
+    return updatePool.slice(0);
   }
 
-  autoRun(func) {
-    if (!this.updatePool.includes(func)) this.updatePool.push(func);
+  function autoRun(func) {
+    if (updatePool.includes(func)) {
+      removeFromAutoRun(func);
+      updatePool.push(func);
+    } else if (checkType(func)) {
+      if (Array.isArray(func)) {
+        func.forEach(funcInArray => autoRun(funcInArray));
+      } else {
+        updatePool.push(func);
+      }
+    }
+    return updatePool.slice(0);
   }
 
-  removeFromAutoRun(func) {
-    this.updatePool.splice(this.updatePool.findIndex(oldFunc => oldFunc === func), 1);
-  }
+  this.update = update.bind(this);
+  this.autoRun = autoRun.bind(this);
+  this.removeFromAutoRun = removeFromAutoRun.bind(this);
+  this.clear = () => { updatePool = []; };
 }
 
 export default FuncPool;
